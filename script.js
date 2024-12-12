6db0bf66e8642821ee51169db7e7d317e7575b6f
newman-csa <logann24@nycstudents.net>
2024-12-11 23:48:47 -0500

    Wewewewe, I'm done for now

--- script.js
+++ script.js
@@ -58,32 +58,104 @@ async function fetchImage(endpointPath) {
   }
 }
 
-let user = null;
-fetchData('https://jsonplaceholder.typicode.com/users/1')
-  .then((data) => {
-    user = data;
-    document.querySelector('#profile-name').textContent = user.name;
-  })
-  .then(console.log);
+async function getProfilePicture() {
+  try {
+    const url = await fetchImage('https://unsplash.it/15/');
+    return url;
+  } catch (err) {
+    console.error(err);
+  }
+}
 
-fetchImage('https://unsplash.it/300/200?random&blur=3')
-  .then((url) => (document.querySelector('#background-image').src = url))
-  .catch(console.error);
+async function renderPosts(endpointPaths) {
+  try {
+    const posts = await fetchMultiple(endpointPaths);
 
-fetchImage('https://unsplash.it/150/?random')
-  .then((url) => (document.querySelector('#profile-picture').src = url))
-  .catch(console.error);
+    const profilePicture = posts.map(() => getProfilePicture());
+    const profilePictures = await Promise.all(profilePicture);
 
-// const endpointPath = 'https://jsonplaceholder.typicode.com/users';
-// const endpointPaths = Array(3)
-//   .fill(null)
-//   .map((_, i) => `${endpointPath}/${i + 1}`);
+    const profileName = posts.map((post) =>
+      fetchData(`https://jsonplaceholder.typicode.com/users/${post.id}`).then(
+        (user) => user.name
+      )
+    );
+    const profileNames = await Promise.all(profileName);
 
-// fetchMultiple(endpointPaths)
-//   .then((data) => data.map((user) => user.address))
-//   .then(
-//     (addresses) =>
-//       (document.querySelector('#main-content').lastChild.textContent =
-//         JSON.stringify(addresses))
-//   )
-//   .catch(console.error);
+    for (const [index, post] of posts.entries()) {
+      const url = profilePictures[index];
+      const name = profileNames[index];
+
+      const div = document.createElement('div');
+
+      div.classList.add('post');
+      div.innerHTML = `
+      <h3>${capitalize(
+        post.title
+      )} <span class="post-author">by <img src="${url}" alt="Profile Picture"> ${name}</span></h3>
+      <p>${capitalize(post.body)}</p>
+      <div class="post-actions">
+      <button type="button"><i class="fa-solid fa-comments"></i> Comments</button>
+      <button type="button"><i class="fa-solid fa-share"></i> Share</button>
+      </div>
+      `;
+      document.querySelector('#main-content').appendChild(div);
+      document
+        .querySelector('#main-content')
+        .appendChild(document.createElement('hr'));
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function capitalize(text) {
+  return String(text).charAt(0).toUpperCase() + String(text).slice(1);
+}
+
+function initMap(x, y) {
+  let map = L.map('map').setView([x, y], 13);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  fetchData('https://jsonplaceholder.typicode.com/users/1')
+    .then((user) => {
+      console.log(user);
+
+      document.querySelector('#profile-name').textContent = user.name;
+      document.querySelector(
+        '#profile-contact'
+      ).textContent = `${user.phone} ・ ${user.email} ・ ${user.website}`;
+      document.querySelector(
+        '#profile-address'
+      ).textContent = `${user.address.street}, ${user.address.suite}, ${user.address.city}, ${user.address.zipcode}`;
+      initMap(53.3995432, -0.7728464);
+    })
+    .catch(console.error);
+
+  fetch('/assets/profile-desc.txt')
+    .then((response) => response.text())
+    .then((text) => {
+      document.querySelector('#profile-bio').textContent = text;
+    })
+    .catch(console.error);
+
+  fetchImage('https://unsplash.it/300/200?random&blur=3')
+    .then((url) => (document.querySelector('#background-image').src = url))
+    .catch(console.error);
+
+  fetchImage('https://unsplash.it/150/?random')
+    .then((url) => (document.querySelector('#profile-picture').src = url))
+    .catch(console.error);
+
+  const endpointPath = 'https://jsonplaceholder.typicode.com/posts';
+  const endpointPaths = Array(10)
+    .fill(null)
+    .map((_, i) => `${endpointPath}/${i + 1}`);
+
+  renderPosts(endpointPaths);
+});
